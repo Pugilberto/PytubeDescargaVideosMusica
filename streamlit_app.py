15739e19711f9a9a5cbe406371283d0105093d1e
Pugilberto <77805945+Pugilberto@users.noreply.github.com>
2024-08-13 20:45:44 -0600

    Update streamlit_app.py first try

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from pytube import YouTube
+from pytube import Playlist
+import os
+from moviepy.editor import *
+import time
+
+from pytube.innertube import _default_clients
+from pytube import cipher
+import re
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+st.write(
+    "pugilberto its back"
 )
